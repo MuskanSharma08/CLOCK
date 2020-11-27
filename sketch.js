99d2d75043336acb50016364264f87842a2f3aea
Muskan Sharma <70201126+MuskanSharma08@users.noreply.github.com>
2020-11-27 16:31:24 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+var hr, mn, sc;
+var hrAngle, mnAngle, scAngle;
+
+function setup() {
+  createCanvas(400,400);
+  angleMode(DEGREES);
+}
+
+function draw() {
+  background(0);  
+  hr = hour();
+  mn = minute();
+  sc = second();
+  
+  translate(200, 200);
+  rotate(-90);
+
+  hrAngle = map(hr % 12, 0, 12, 0, 360);
+  mnAngle = map(mn, 0, 60, 0, 360);
+  scAngle = map(sc, 0, 60, 0, 360);
+
+
+  push();
+  rotate(hrAngle);
+  stroke("red");
+  strokeWeight(7);
+  line(0, 0, 50, 0);
+  pop()
+
+  push();
+  rotate(mnAngle);
+  stroke("yellow");
+  strokeWeight(7);
+  line(0, 0, 75, 0);
+  pop()
+
+  push();
+  rotate(scAngle);
+  stroke("blue");
+  strokeWeight(7);
+  line(0, 0, 100, 0);
+  pop();
+
+  strokeWeight(10);
+  stroke("red");
+  noFill();
+  arc(0, 0, 260, 260, 0, hrAngle)
+
+  strokeWeight(10);
+  stroke("yellow");
+  noFill();
+  arc(0, 0, 280, 280, 0, mnAngle)
+
+  strokeWeight(10);
+  stroke("blue");
+  noFill();
+  arc(0, 0, 300, 300, 0, scAngle)
+}
